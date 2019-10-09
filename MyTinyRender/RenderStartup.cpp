fb4f1f3666a6b37da46b5b51f0b3507d69054aac
rogerygluo <rogerygluo@tencent.com>
2019-10-09 18:03:05 +0800

    add project

---
+++ MyTinyRender/RenderStartup.cpp
@@ -0,0 +1,198 @@
+#include "tgaimage.h"
+#include "model.h"
+#include "geometry.h"
+#include "Triangulate.h"
+
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor green = TGAColor(0, 255, 0, 255);
+void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color);
+
+void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color)
+{
+	bool steep = false;
+	if (std::abs(x0-x1)<std::abs(y0-y1))
+	{
+		std::swap(x0, y0);
+		std::swap(x1, y1);
+		steep = true;
+	}
+	int step = x1 > x0 ? 1 : -1;
+	float ratio = 1 / (float)(x1 - x0);
+	for (int x = x0;  x!=x1; x += step)
+	{
+		float t = (x - x0) * ratio;
+		int y = y0  + (y1 - y0)* t;
+		if (steep) {
+			image.set(y, x, color);
+		}
+		else {
+			image.set(x, y, color);
+		}		
+	}
+}
+void line(Vec2i v0, Vec2i v1, TGAImage& image, TGAColor color)
+{
+	line(v0.x, v0.y, v1.x, v1.y, image, color);
+}
+
+void FillTriangle(Vec2i v0, Vec2i v1, Vec2i v2, TGAImage& image, TGAColor color)
+{
+	
+	if (v0.y > v1.y) std::swap(v0, v1);
+	if (v0.y > v2.y) std::swap(v0, v2);
+	if (v1.y > v2.y) std::swap(v1, v2);
+	int totalH = v2.y - v0.y;
+	for (int i = v0.y; i <= v2.y; i++)
+	{
+		bool bSecond = i > v1.y || v1.y == v0.y;
+		Vec2i start = v0 + (v2 - v0) * ((float)(i-v0.y) / totalH);
+		Vec2i end = bSecond ? (v1 + (v2-v1)*((float)(i-v1.y))/(v2.y - v1.y)) : (v0 + (v1-v0)*((float)(i-v0.y)/(v1.y - v0.y)));
+		if (start.x > end.x) std::swap(start, end);
+		for (int j = start.x; j <= end.x; j++)
+		{
+			image.set(j, i, color);
+		}
+	}
+	 
+	//line(v0, v1, image, green);
+	//line(v1, v2, image, white);
+	//line(v2, v0, image, red);
+}
+
+void DrawBasicLines()
+{
+	TGAImage image(100, 100, TGAImage::RGB);
+	//image.set(52, 41, red);
+	line(10, 4, 50, 30, image, white);
+	line(50, 30, 10, 7, image, red);
+	line(20, 13, 40, 80, image, red);
+	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+	image.write_tga_file("output.tga");
+}
+
+void DrawMesh(const char* meshName, int width, int height)
+{
+	if (meshName == nullptr) {
+		return;
+	}
+	Model headModel(meshName);
+	TGAImage headImage(width, height, TGAImage::RGB);
+	for (int i = 0; i < headModel.nfaces(); i++)
+	{
+		std::vector<int> face = headModel.face(i);
+		for (int j = 0; j < 3; j++)
+		{
+			Vec3f v0 = headModel.vert(face[j]);
+			Vec3f v1 = headModel.vert(face[(j + 1) % 3]);
+			int x0 = (v0.x + 1)*width / 2.0f;
+			int y0 = (v0.y + 1)*height / 2.0f;
+			int x1 = (v1.x + 1)*width / 2.0f;
+			int y1 = (v1.y + 1)*height / 2.0f;
+			line(x0, y0, x1, y1, headImage, white);
+		}
+	}
+	headImage.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+	std::string name(meshName);
+	headImage.write_tga_file((name.substr(0, name.length() - 4) + ".tga").c_str());
+}
+
+void FillMesh(const char* meshName, int width, int height)
+{
+	if (meshName == nullptr) {
+		return;
+	}
+	Model headModel(meshName);
+	TGAImage headImage(width, height, TGAImage::RGB);
+	Triangulate tri;
+	
+	Vec2i v[3];
+	Vec3f world[3];
+	Vec3f light_dir(0,0,-1);
+	for (int i = 0; i < headModel.nfaces(); i++)
+	{
+		std::vector<int> face = headModel.face(i);		
+		for (int j = 0; j < 3; j++)
+		{
+			world[j] = headModel.vert(face[j]);
+			Vec3f& v0 = world[j];
+			v[j].x = (v0.x + 1)*width / 2.0f;
+			v[j].y = (v0.y + 1)*height / 2.0f;
+		}
+		tri.ResetVertex(v[0], v[1], v[2]);
+		Vec3f n = cross(world[2] - world[0], world[1] - world[0]);
+		n.normalize();
+		float intensity = n*light_dir;
+		if (intensity > 0)
+		{
+			tri.DoTriangulate(headImage, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
+		}
+		
+	}
+	headImage.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+	std::string name(meshName);
+	headImage.write_tga_file((name.substr(0, name.length() - 4) + ".tga").c_str());
+}
+
+void FillMeshZTest(const char* meshName, int width, int height)
+{
+	if (meshName == nullptr) {
+		return;
+	}
+	Model headModel(meshName);
+	TGAImage headImage(width, height, TGAImage::RGB);
+	Triangulate tri;
+
+	Vec2i v[3];
+	Vec3f world[3];
+	Vec2f uv[3];
+	Vec3f light_dir(0, 0, -1);
+	TGAColor uvColor[3];
+	float* zBuffer = new float[width*height];
+	for (int i = width * height; i--; zBuffer[i] = -std::numeric_limits<float>::max());
+	for (int i = 0; i < headModel.nfaces(); i++)
+	{
+		std::vector<int> face = headModel.face(i);
+		for (int j = 0; j < 3; j++)
+		{
+			world[j] = headModel.vert(face[j]);
+			Vec3f& v0 = world[j];
+			v[j].x = (v0.x + 1)*width / 2.0f;
+			v[j].y = (v0.y + 1)*height / 2.0f;
+			uv[j] = headModel.uv(i, j);
+			uvColor[j] = headModel.diffuse(uv[j]);
+		}
+		tri.ResetVertex(v[0], v[1], v[2]);
+		Vec3f n = cross(world[2] - world[0], world[1] - world[0]);
+		n.normalize();
+		float intensity = n * light_dir;
+		if (intensity > 0) // do back culling
+		{
+			//tri.DoTriangulate(headImage, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
+			tri.DoTriangulateZTest(headImage, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255), zBuffer, world[0].z, world[1].z, world[2].z);
+		}
+
+	}
+	headImage.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+	std::string name(meshName);
+	headImage.write_tga_file((name.substr(0, name.length() - 4) + ".tga").c_str());
+	delete[] zBuffer;
+}
+
+int main(int argc, char** argv) {
+	
+// 	DrawBasicLines();
+// 	DrawMesh("african_head.obj", 800, 800);	
+// 	TGAImage image(400, 400, TGAImage::RGB);
+// 	FillTriangle(Vec2i(10, 10), Vec2i(300, 270), Vec2i(220, 355), image, green);
+// 	image.flip_vertically();
+// 	image.write_tga_file("triangle.tga");
+// 
+// 	TGAImage newImage(400, 400, TGAImage::RGB);
+// 	Triangulate tri(Vec2i(10, 10), Vec2i(300, 270), Vec2i(220, 355));
+// 	tri.DoTriangulate(newImage, red);
+// 	newImage.flip_vertically();
+// 	newImage.write_tga_file("newTri.tga");
+	FillMeshZTest("african_head.obj", 800, 800);
+	return 0;
+}
---
+++ MyTinyRender/Triangulate.h
@@ -0,0 +1,101 @@
+#pragma once
+#include "geometry.h"
+#include "tgaimage.h"
+#include <algorithm>
+
+class Triangulate
+{
+public:
+	Triangulate()
+	{
+		mTriangles.resize(3, Vec2i(0, 0));
+	}
+	Triangulate(Vec2i a, Vec2i b, Vec2i c)
+	{
+// 		if (a.y > b.y) std::swap(a, b);
+// 		if (a.y > c.y) std::swap(a, c);
+// 		if (b.y > c.y) std::swap(b, c);
+		mTriangles.resize(3);
+		mTriangles[0] = a;
+		mTriangles[1] = b;
+		mTriangles[2] = c;
+	}
+	Vec2i& GetA() { return mTriangles[0]; }
+	Vec2i& GetB() { return mTriangles[1]; }
+	Vec2i& GetC() { return mTriangles[2]; }
+
+	Vec3f GetBerycentric(Vec2i p)
+	{
+		Vec3i u = cross(Vec3i(mTriangles[1].x - mTriangles[0].x, mTriangles[2].x - mTriangles[0].x, mTriangles[0].x - p.x), Vec3i(mTriangles[1].y - mTriangles[0].y, mTriangles[2].y - mTriangles[0].y, mTriangles[0].y - p.y));
+		if (u.z == 0) {
+			return Vec3f(-1, 1, 1);
+		}
+		return Vec3f(1.0f - (u.x + u.y) / (float)(u.z), u.x/(float)u.z, u.y/(float)u.z);
+	}
+
+	void ResetVertex(Vec2i a, Vec2i b, Vec2i c)
+	{
+		if (mTriangles.size() != 3)
+		{
+			mTriangles.resize(3);			
+		}
+		mTriangles[0] = a;
+		mTriangles[1] = b;
+		mTriangles[2] = c;
+	}
+	void DoTriangulate(TGAImage& image, TGAColor color)
+	{
+		Vec2i bboxmin(image.get_width() - 1, image.get_height() - 1);
+		Vec2i bboxmax(0, 0);
+		for (int i = 0; i < 3; i++)
+		{
+			bboxmin.x = std::max<int>(0, std::min<int>(bboxmin.x, mTriangles[i].x));
+			bboxmin.y = std::max<int>(0, std::min<int>(bboxmin.y, mTriangles[i].y));
+
+			bboxmax.x = std::min<int>(image.get_width() -1, std::max<int>(bboxmax.x, mTriangles[i].x));
+			bboxmax.y = std::min<int>(image.get_height() - 1, std::max<int>(bboxmax.y, mTriangles[i].y));
+		}
+		Vec2i p;
+		for (p.x = bboxmin.x; p.x <= bboxmax.x; p.x++)
+		{
+			for (p.y = bboxmin.y; p.y <= bboxmax.y; p.y++)
+			{
+				Vec3f baryPos = GetBerycentric(p);
+				if (baryPos.x < 0 || baryPos.y < 0 || baryPos.z < 0) { continue; }
+				image.set(p.x, p.y, color);
+			}
+		}
+	}
+	void DoTriangulateZTest(TGAImage& image, TGAColor color, float* zBuffer, float z1, float z2, float z3)
+	{
+		Vec2i bboxmin(image.get_width() - 1, image.get_height() - 1);
+		Vec2i bboxmax(0, 0);
+		for (int i = 0; i < 3; i++)
+		{
+			bboxmin.x = std::max<int>(0, std::min<int>(bboxmin.x, mTriangles[i].x));
+			bboxmin.y = std::max<int>(0, std::min<int>(bboxmin.y, mTriangles[i].y));
+
+			bboxmax.x = std::min<int>(image.get_width() - 1, std::max<int>(bboxmax.x, mTriangles[i].x));
+			bboxmax.y = std::min<int>(image.get_height() - 1, std::max<int>(bboxmax.y, mTriangles[i].y));
+		}
+		Vec2i p;
+		float z = 0;
+		for (p.x = bboxmin.x; p.x <= bboxmax.x; p.x++)
+		{
+			for (p.y = bboxmin.y; p.y <= bboxmax.y; p.y++)
+			{
+				Vec3f baryPos = GetBerycentric(p);
+				if (baryPos.x < 0 || baryPos.y < 0 || baryPos.z < 0) { continue; }
+				z = baryPos.x*z1 + baryPos.y*z2 + baryPos.z*z3;
+				if (z > zBuffer[p.x*image.get_height() + p.y])
+				{
+					zBuffer[p.x*image.get_height() + p.y] = z;
+					image.set(p.x, p.y, color);
+				}
+				
+			}
+		}
+	}
+private:
+	std::vector<Vec2i> mTriangles;
+};
